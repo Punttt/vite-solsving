4cfd1e54e8b24e7358eb8d4a88384bc77f5fe934
Pontus Johansson <johanssonp998@gmail.com>
2026-03-05 10:51:23 +0100

    notera JS koden

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ let lat;
 let lng;
 
 
+/**
+ * Inväntar DOM att bli färdigladdat
+ */
 document.addEventListener("DOMContentLoaded", ()=>{
     const form = document.getElementById("locationForm");
     const input = document.getElementById("locationInput");
@@ -12,6 +15,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const query = input.value;
         console.log(query);
 
+        // Anropar funktion för att hämta koordinater för plats
         await searchLocation(query);
     })
 })
@@ -19,13 +23,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 /**
  * Hämtar koordinater från Google Geocoding API baserat på en söksträng.
+ * 
+ * @async
  * @param {string} query - Platsen som användaren söker efter.
+ * @returns {Promise<void>}
  */
 async function searchLocation(query) {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(query)}&key=AIzaSyC4Gx-KoIj3bIpmEd9Q9AkETl8ZJxRVzAI`;
-    const data;
+    let data;
 
     try{
+        const response = await fetch(url);
+        data = await response.json();
+
+        if(data.status != "OK"){
+            console.error(data.error_message);
+            return;
+        }
+
+        lat = data.results[0].geometry.location.lat;
+        lng = data.results[0].geometry.location.lng;
+
+        // Skriver ut för test
+        console.log(`Lat: ${lat}`);
+        console.log(`Lng: ${lng}`);
 
     }catch (error){
         console.error(error);
